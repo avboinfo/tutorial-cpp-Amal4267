779ed5f2f9291b5c93b5406b6958c1fd47610769
Amin Itrbi <152690464+Amal4267@users.noreply.github.com>
2024-04-23 17:29:28 +0000

    23/04/2024

--- X_BattleShip/Battleship.cpp
+++ X_BattleShip/Battleship.cpp
@@ -9,66 +9,49 @@ Itrbi Mohamed Amin - 22/04/2024
 using namespace std;
 
 
-class BattleShip{
+
+class BattleShip {
 
     private:
+
     BattleField mappa;
     BattleField campo;
 
-
-    public: 
-    BattleShip(){
-        srand(time(NULL));
-        mappa = BattleField( VOID );
-        campo = BattleField( VOID );
-        campo.placeHorizontalShip(3); 
+    public:
+    BattleShip() {
+        mappa = BattleField(VOID);
+        campo = BattleField(VOID);
+        campo.placeHorizontalShip(3);
         campo.placeVerticalShip(4);
         campo.placeVerticalShip(2);
         campo.placeHorizontalShip(5);
-        
-
     }
 
-    void play(){
-
-        while(true){
+    void play() {
+        while ( ! gameOver() ) {
             mappa.stampa();
-            if( !playHand()) 
-            break;
-  
+            if (!playHand()) break;
         }
-
-        
         campo.stampa();
     }
 
-    bool playHand(){
-        cout<<"Inserisci le cordinate di riga e colona (1-10) in cui sganciare la bomba";
+    bool playHand() {
+        cout << "Inserisci le coordinate di riga e colonna (1 - " << DIM << ") in cui sganciare la bomba (0 per terminare): ";
         int x, y;
-        cin>>x; if (x == 0 || x > DIM) return false; else x--;
-        cin>>y; if (y == 0 || y > DIM) return false; else y--;
-        if(x<=0)
-            return false; else x--;
-        if(y<=0)
-            return false; else y--;
-            
-        
-        if(campo.get(x,y)==SHIP) {
-                mappa.put(x,y,HIT);
-                campo.put(x,y,HIT);
-            } else mappa.put(x,y,MISS);
-        }
-    bool Gameover(){
-            
-            for (int i = 0; i < DIM; i++) {
-                for (int j = 0; j < DIM; j++) {
-                    if (campo.get(i, j) == SHIP) {
-                        return false;
-                    
-                }
-            }
-            return true;
-        }
+        cin >> x; if (x<=0 || x>DIM) return false; else x--;
+        cin >> y; if (y<=0 || y>DIM) return false; else y--;
+        if (campo.get(x,y)==SHIP) {
+            mappa.put(x,y,HIT);
+            campo.put(x,y,HIT);
+        } else mappa.put(x,y,MISS);
+        return true;
+    }
 
-        }
-    };
+    bool gameOver() {
+        for (int i=0; i<DIM; i++) 
+          for (int j=0; j<DIM; j++)
+            if (campo.get(i,j)==SHIP) return false;
+        return true;
+    }
+
+};
